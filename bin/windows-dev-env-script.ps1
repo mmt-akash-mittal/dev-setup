e14c2055f51dfeeae726788ed82dce46493332df
mmt-akash-mittal <mittala@media-saturn.com>
2025-11-04 11:43:32 +0100

    Refactor comments and improve script readability

--- bin/windows-dev-env-script.ps1
+++ bin/windows-dev-env-script.ps1
@@ -1,80 +1,79 @@
+# ----------------------------------------
 # Ensure Scoop is installed
-# Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+# ----------------------------------------
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 # Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-# Add necessary buckets
+# ----------------------------------------
+# Add necessary Scoop buckets
+# ----------------------------------------
 scoop bucket add versions
 scoop bucket add extras
 scoop bucket add main
 scoop bucket add java
 
-
-
-# -------------------------------
+# ----------------------------------------
 # Install Git (if not installed)
-# -------------------------------
+# ----------------------------------------
 if (-not (scoop list | Select-String -Pattern "^git$")) {
     scoop install git
 } else {
     Write-Host "Git is already installed. Skipping installation."
 }
 
-
-# -------------------------------
-# Install Java 21
-# -------------------------------
+# ----------------------------------------
+# Install Java 21 (OpenJDK)
+# ----------------------------------------
 if (-not (scoop list | Select-String -Pattern "^java$")) {
-	scoop install openjdk21
+    scoop install openjdk21
 } else {
     Write-Host "Java is already installed. Skipping installation."
 }
 
-
-# -------------------------------
-# Install Maven 3.2.2
-# -------------------------------
+# ----------------------------------------
+# Install Maven
+# ----------------------------------------
 if (-not (scoop list | Select-String -Pattern "^maven$")) {
-	scoop install maven
+    scoop install maven
 } else {
     Write-Host "Maven is already installed. Skipping installation."
 }
 
-# -------------------------------
-# Install nodejs default the latest version, lts viz 22 currently or any specific versions
-# -------------------------------
-
+# ----------------------------------------
+# Install Node.js (LTS version)
+# ----------------------------------------
 if (-not (scoop list | Select-String -Pattern "^nodels$")) {
-	scoop install nodejs-lts
-	# scoop install nodejs
-	# scoop install nodejs --version="22.20.0"
+    scoop install nodejs-lts
+    # scoop install nodejs
+    # scoop install nodejs --version="22.20.0"
 } else {
-    Write-Host "Node JS is already installed. Skipping installation."
+    Write-Host "Node.js is already installed. Skipping installation."
 }
-# -------------------------------
-# Install node version manager to able to switch between several version
-# NVM Commands cheat sheat - https://gist.github.com/chranderson/b0a02781c232f170db634b40c97ff455
-# -------------------------------
 
+# ----------------------------------------
+# Install Node Version Manager (NVM)
+# ----------------------------------------
+# NVM Commands cheat sheet: https://gist.github.com/chranderson/b0a02781c232f170db634b40c97ff455
 if (-not (scoop list | Select-String -Pattern "^nvm$")) {
-	scoop install nvm
+    scoop install nvm
 } else {
-    Write-Host "Node Version Manager (mvn) is already installed. Skipping installation."
+    Write-Host "Node Version Manager (NVM) is already installed. Skipping installation."
 }
 
-# -------------------------------
-# Installs Latest version of IntelliJ - trial version
-# ------------------------------------
+# ----------------------------------------
+# Install IntelliJ IDEA Ultimate (Trial)
+# ----------------------------------------
 if (-not (scoop list | Select-String -Pattern "^idea$")) {
-	scoop install idea-ultimate
+    scoop install idea-ultimate
 } else {
-    Write-Host "Intellij is already installed. Skipping installation."
+    Write-Host "IntelliJ IDEA is already installed. Skipping installation."
 }
 
-
-
+# ----------------------------------------
+# Install Google Cloud SDK (gcloud)
+# ----------------------------------------
 if (-not (scoop list | Select-String -Pattern "^gcloud$")) {
-scoop install gcloud
+    scoop install gcloud
 } else {
-    Write-Host "Google Cloud is already installed. Skipping installation."
+    Write-Host "Google Cloud SDK is already installed. Skipping installation."
 }
-
